d0ffd351f52cc8837a0c04e46a19754c13b646f8
Gurinder Singh <92389887+GurinderRawala@users.noreply.github.com>
2022-04-16 01:14:27 -0400

    background-clip css updated

--- src/components/welcome-section/welcome-buttons.js
+++ src/components/welcome-section/welcome-buttons.js
@@ -3,7 +3,7 @@ import React from 'react'
 const WelcomeButtons = () =>{
     return(
       <div data-aos="fade-right" className='App-row-flex col-sm-12' 
-      style={{justifyContent: 'space-evenly', marginTop: 20}}>
+      style={{justifyContent: 'space-evenly'}}>
           <button>I'M NEW TO GREWAL</button>
           <button>CONTACT GREWAL</button>
       </div>
